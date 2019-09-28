088f932fa36139e45fad60c80f9856ccd2f8dd10
NLshaen <yan.lucas@free.fr>
2019-09-28 18:15:08 +0200

    add bot function start stop status

--- core.lua
+++ core.lua
@@ -1,20 +1,41 @@
-# Print message on connect
+-- Print message on connect
 message("You are not alone !!!")
 
-local EventFrame = CreateFrame("Frame")
+local frame = CreateFrame("Frame")
 
-EventFrame:RegisterEvent("PLAYER_LOGIN")
+-- frame:RegisterEvent("PLAYER_LOGIN")
 
-EventFrame:SetScript("OnEvent", function(self,event,...)
-	if type(CharacterVar) ~= "number" then
-		CharacterVar = 1
-		ChatFrame1:SendChatMessage('[Bot_S] '.. UnitName("Player")..". I do believe this is the first time we've met. Nice to meet you!","GUILD")
-	else
-		if CharacterVar == 1 then
-			ChatFrame1:SendChatMessage('[Bot_S] '.. UnitName("Player")..". How nice to see you again. I do believe I've seen you " .. CharacterVar .. " time before.","GUILD")
-		else
-			ChatFrame1:SendChatMessage('[Bot_S] '.. UnitName("Player")..". How nice to see you again. I do believe I've seen you " .. CharacterVar .. " times before.","GUILD")
-		end
-		CharacterVar = CharacterVar + 1
-	end
+frame:RegisterEvent("CHAT_MSG_GUILD")
+
+frame:SetScript("OnEvent", function(text, author)
+    bot:onGuildMessage(text, author)
 end)
+
+function bot:spam(text)
+    SendChatMessage(text, "GUILD")
+end
+
+function bot:onGuildMessage(message, author)
+    if message == '!bot_start' then
+        self:start()
+    elseif message == '!bot_status' then
+        self:status()
+    elseif message == '!bot_stop' then
+        self:stop()
+    end
+end
+
+function bot:start()
+    self:spam("[BOT_S] Actived : J'aime quand tu m'actives !!!")
+end
+
+function bot:stop()
+    self:spam("[BOT_S] Stopped : Adieu veaux vaches cochons !!! ")
+end
+
+function bot:status()
+    self:spam("[BOT_S] Status : Je suis toujours là !!!")
+end
+
+-- end)
+-- CHAT_MSG_GUILD
